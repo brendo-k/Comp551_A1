e5d21ef6317a3090eec71e52ae94b35b7b30f32e
Thomas Wright <50252341+thomasgudjonwright@users.noreply.github.com>
2020-02-09 17:32:01 -0500

    Changing functions to work more intuitively:

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -15,33 +15,47 @@ class NaiveBayes():
         self.X = X
         self.Y = Y
     
-    @staticmethod    
-    def fit(summs, row): # copied, needs revision
-        num_instances = np.sum([summs[label][0][2] for label in summs])
+      
+    def fit(summaries, instance, Y): # calculates class probabilities
+        num_instances = np.sum([summaries[label][0][2] for label in summaries])
         probs = dict()
-        for label, class_summs in summs.items():
-            probs[label] = (summs[label][0][2] / float(num_instances))
+        for label, class_summs in summaries.items():
+            probs[label] = (summaries[label][0][2] / float(num_instances))
+            probs[label] = NaiveBayes.probLabel(label, Y)
             for i in range(len(class_summs)):
-                mean, std, count = class_summs[i]
-                probs[label] *= NaiveBayes.like(row[i], mean, std)
+                mu, sigma, count = class_summs[i]
+                feature = instance[i]
+                probs[label] *= NaiveBayes.gaussProb(feature, mu, sigma)
         return probs
     
     @staticmethod
-    def predict(data):
+    def predict(data, Y): # this will give a final prediction
         y = [None]*len(data)
+        summaries = NaiveBayes.summarizeClass(data)
         for i in range(len(data)):
-            probs = NaiveBayes.fit(NaiveBayes.classSumms(data), data[i])
-            if probs[0]>=probs[1]:
-                y[i] = 0
-            else:
-                y[i] = 1
+            instance = data[i]
+            instanceProbs = NaiveBayes.fit(summaries, instance, Y)
+            bestLabel, bestProb = None, -1.0
+            for label, prob in instanceProbs.items():
+                if (bestLabel is None) or (prob > bestProb):
+                    bestProb = prob
+                    bestLabel = label
+            y[i] = bestLabel
         return y
-     
+        
     @staticmethod    
-    def like(x, mean, std):
+    def gaussProb(x, mean, std):
         exp = np.exp(-((x-mean)**2)/(2 * (std**2)))
         prob = exp * (1 / (np.sqrt(2 * np.pi) * std))
         return prob
+    
+    @staticmethod
+    def probLabel(lab, Y):
+        count = 0
+        for i in range(len(Y)):
+            if Y[i]==lab:
+                count+=1
+        return count/len(Y)
     
     @staticmethod
     def splitData(data): # organizing the data by class (dict: label -> list of instances with that label)
@@ -55,26 +69,27 @@ class NaiveBayes():
         return split
             
     @staticmethod            # creates an array of tuples that summarizes the data:
-    def summData(data): # first el of the tuple is mean, second is std. dev.
+    def summarizeData(data): # first el of the tuple is mean, second is std. dev.
         summs= [(np.mean(col), np.std(col), len(col)) for col in zip(*data)]
         del(summs[-1])
         return summs
     
     @staticmethod
-    def classSumms(data): 
+    def summarizeClass(data): 
         split = NaiveBayes.splitData(data)
         summs = dict()
         for label, instances in split.items():
-            summs[label] = NaiveBayes.summData(instances)
+            summs[label] = NaiveBayes.summarizeData(instances)
         return summs
 
 def main():
-    file = file = open("Ionosphere_Numpy_Array.txt", "r")
+    file = open("Ionosphere_Numpy_Array.txt", "r")
     X = np.loadtxt(file) # data loaded into numpy array
     X_clean = np.delete(X, 1, 1)
     yClassified = X[:, -1] # extracting labels
     X_clean = X_clean[:, 1:] # removing the labels
-    yPred = NaiveBayes.predict(X_clean)
+    yPred = NaiveBayes.predict(X_clean, yClassified)
+
     TP = 0
     FN = 0
     TN = 0
@@ -94,6 +109,5 @@ def main():
     print(" True positive: %d \n False Negative: %d \n True Negative: %d \n False Positive: %d" % (TP, FN, TN, FP))
     print(" Accuracy: %.3f%% " % (acc*100))
     
-if (__name__ == "__main__"):
-    main() # running main 
-    
+if __name__ == "__main__":
+    main()
