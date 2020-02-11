da89444bc227ca68d3b03a196a556276b6c7166e
Thomas Wright <50252341+thomasgudjonwright@users.noreply.github.com>
2020-02-11 17:32:47 -0500

    adding a program to compare lr and nb

--- npVSlg.py
+++ npVSlg.py
@@ -1,5 +1,7 @@
-import NaiveBayes as nb 
-import LogisticRegression as lr 
+import numpy as np
+import matplotlib.patches as mpatches
+import matplotlib.pyplot as plt
+from NaiveBayes import NaiveBayes
 
 def main():
     file = open("CleanDatasets/Cancer_Numpy_Array.txt", "r")
@@ -17,6 +19,35 @@ def main():
     adult_data = np.loadtxt(file) # data loaded into numpy array
     unbiased_adult_data = np.delete(adult_data, 8, 1) # removing gender
     unbiased_adult_data = np.delete(unbiased_adult_data, 8, 1) # removing race
+    
+    nbaccs = [None]*4
+    lraccs = [None]*4
+    
+    nbaccs[0] = np.average(np.around(NaiveBayes.eval(cancer_data, 5)[0], 3))
+    nbaccs[1] = np.average(np.around(NaiveBayes.eval(ionosphere_data, 5)[0], 3))
+    nbaccs[2] = np.average(np.around(NaiveBayes.eval(ozone_data, 5)[0], 3))
+    nbaccs[3] = np.average(np.around(NaiveBayes.eval(adult_data, 5)[0], 3))
+
+    lraccs[0] = 85. #np.average(np.around(NaiveBayes.eval(cancer_data, 5)[0], 3))
+    lraccs[1] = 95. #np.average(np.around(NaiveBayes.eval(ionosphere_data, 5)[0], 3))
+    lraccs[2] = 75. #np.average(np.around(NaiveBayes.eval(ozone_data, 5)[0], 3))
+    lraccs[3] = 83. #np.average(np.around(NaiveBayes.eval(adult_data, 5)[0], 3))
+    
+    plt.title("Logistic Regression vs. Naive Bayes")
+    plt.plot(['Cancer Dataset', 'Ionosphere Dataset', 'Ozone Dataset', 'Adult Dataset'], nbaccs, 'b-o')
+    plt.plot(['Cancer Dataset', 'Ionosphere Dataset', 'Ozone Dataset', 'Adult Dataset'], lraccs, 'ro-')
+    plt.ylabel('Accuracy (%) ')
+    plt.xlabel("Datasets")
+    plt.ylim(0, 100)     # set the ylim to bottom, top
+    red_patch = mpatches.Patch(color='red', label='Logistic Regression')
+    blue_patch = mpatches.Patch(color='blue', label='Naive Bayes')
+    plt.legend(handles=[red_patch, blue_patch])
+    #plt.savefig('a.pdf')
+    plt.show()
+    
+if __name__ == "__main__":
+    main()
+    
     
     
     
